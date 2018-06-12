8ecc274c8f61435900d1459b1fad9746a661c4e1
Bradley Hudson <bradrayhudson@gmail.com>
2018-06-12 14:16:59 -0400

    add a route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/surveys', to: 'surveys#index'
+  get '/surveys/new', to: 'surveys#new'
+  post '/surveys', to: 'surveys/create'
 end
